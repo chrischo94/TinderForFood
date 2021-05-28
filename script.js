e71f8db9fa166c48c708bf6b49073c12ad087593
Alejandro Gonzalvez <agonzalvez1@gmail.com>
2021-05-28 13:59:12 -0400

    Autocomplete added to search bar

--- script.js
+++ script.js
@@ -10,32 +10,63 @@ function getLocation() {
 function showPosition(position) {
   lat = position.coords.latitude;
   lng = position.coords.longitude;
-  console.log(lat, lng) 
+  console.log(lat, lng)
   getFood();
 }
 getLocation();
 
 
-function getFood(){
-        let queryYelp = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=restaurants&latitude='+ lat + '&longitude=' + lng;
-            console.log(queryYelp)
-            $.ajax({
-              'url': queryYelp,
-              'method': 'GET',
-              'timeout': 0,
-              'headers': {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer CvM2gOH36ZGu00rvjEGg51Nqgc01vaU8dCcxJFwq1GdUOIi9oQCwiokgJHEM_QVF6X26RbOSnpiEZCMSJnfQmVf6q0POwjjQQRoa8Xai26aWHn-xlgcMa5XDRvKvYHYx'
-              },
-            }).then(function (response) {
-              console.log(response)
-            })
-            .catch(function(err) {
-                console.error(err);
-            });
-        }
+function getFood() {
+  let queryYelp = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=restaurants&latitude=' + lat + '&longitude=' + lng;
+  console.log(queryYelp)
+  $.ajax({
+    'url': queryYelp,
+    'method': 'GET',
+    'timeout': 0,
+    'headers': {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer CvM2gOH36ZGu00rvjEGg51Nqgc01vaU8dCcxJFwq1GdUOIi9oQCwiokgJHEM_QVF6X26RbOSnpiEZCMSJnfQmVf6q0POwjjQQRoa8Xai26aWHn-xlgcMa5XDRvKvYHYx'
+    },
+  }).then(function (response) {
+    console.log(response)
+  })
+    .catch(function (err) {
+      console.error(err);
+    });
+}
 
 
+// Search bar autocomplete properties
+
+$(function () {
+  var availableTags = [
+    "Tacos",
+    "Burritos",
+    "Breakfast",
+    "Healthy",
+    "Fried chicken",
+    "Beer",
+    "Cocktails",
+    "Mexican",
+    "Sushi",
+    "Thai",
+    "Italian",
+    "Dessert",
+    "Chocolate",
+    "Vanilla",
+    "Ice cream",
+    "Steak",
+    "Fast food",
+    "Indian",
+    "Vegan",
+    "Cuban",
+    "Falafel",
+    "Seafood"
+  ];
+  $(".form-control").autocomplete({
+    source: availableTags
+  });
+});
 
 
 // Get the modal
@@ -48,18 +79,18 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-} 
+}
